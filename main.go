d7cd75bdfdaa4a23b198428fe83bc3cbf4f3d405
3031208 <21@qwe>
2025-11-14 11:12:46 +0900

    a2

--- main.go
+++ main.go
@@ -2,25 +2,19 @@
 package main
 
 import (
-	"Ruoyi-Go-PocketBase/models"
 	"Ruoyi-Go-PocketBase/service"
 	"fmt"
-	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.New()
-
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		// 获取用户权限菜单树
-		se.Router.GET("/api/system/menu/tree", func(e *core.RequestEvent) error {
+		se.Router.GET("/api/getUserRouter", func(e *core.RequestEvent) error {
 			info, err := e.RequestInfo()
 			// 获取用户角色
 			roleIdsRaw := info.Auth.Get("role")
@@ -41,12 +35,8 @@ func main() {
 				"data":    menuTree,
 			})
 		})
-
-		// 获取所有菜单树（管理员使用）
-		se.Router.GET("/api/system/menu/tree/all", func(e *core.RequestEvent) error {
-			info, err := e.RequestInfo()
-			// 可以在这里添加管理员权限检查
-			_ = info // 使用 info 进行权限验证
+		se.Router.GET("/api/getAllMenuTree", func(e *core.RequestEvent) error {
+			// 调用服务获取菜单树
 			menuService := services.NewMenuService(app)
 			menuTree, err := menuService.GetAllMenuTree()
 			if err != nil {
@@ -62,32 +52,9 @@ func main() {
 				"data":    menuTree,
 			})
 		})
-
-		// 原有的 settings 接口
-		se.Router.GET("/api/myapp/settings", func(e *core.RequestEvent) error {
-			info, _ := e.RequestInfo()
-			roleIdsRaw := info.Auth.Get("role")
-			roleIds := parseRoleIds(roleIdsRaw)
-
-			roles := []models.SysRole{}
-			err := app.DB().
-				Select("*").
-				From("sys_role").
-				Where(dbx.In("id", roleIds)).
-				All(&roles)
-
-			fmt.Println(roles, err)
-
-			return e.JSON(http.StatusOK, map[string]any{
-				"roleIds": roleIds,
-				"roles":   roles,
-			})
-		})
-
-		se.Router.GET("/", apis.Static(os.DirFS("./pb_public/index.html"), false))
+		//se.Router.GET("/", apis.Static(os.DirFS("./pb_public/index.html"), false))
 		return se.Next()
 	})
-
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
@@ -96,7 +63,6 @@ func main() {
 // parseRoleIds 解析角色ID
 func parseRoleIds(roleIdsRaw any) []string {
 	var roleIds []string
-
 	switch v := roleIdsRaw.(type) {
 	case string:
 		if v != "" {
@@ -113,6 +79,5 @@ func parseRoleIds(roleIdsRaw any) []string {
 	default:
 		fmt.Println("unknown role type:", v)
 	}
-
 	return roleIds
 }
